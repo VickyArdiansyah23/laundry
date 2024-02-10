b67051e5ead570b7db2ace4952688cd0a6428843
VickyArdiansyah23 <149611009+VickyArdiansyah23@users.noreply.github.com>
2024-02-10 18:19:26 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost","root","","vicky_laundry");
-
-// Check connection
-if (mysqli_connect_errno()){
-    echo"koneksi database gagal : " . mysqli_connect_error();
-}
-?>
